af9834fc713ccfc076471ef59804f40122d6c2b2
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-12-16 17:41:19 +0700

    meyesuaikan nama tag

--- src/components/UI/tagColor.jsx
+++ src/components/UI/tagColor.jsx
@@ -7,11 +7,6 @@ export const tagColor = {
   react: { bgColor: "#b2475dE2", hover: "#933346E2" },
   tutorial: { bgColor: "#865DCAE2", hover: "#784cc4E2" },
   html: { bgColor: "#f06529", hover: "#e34c26" },
-  nodejs: { bgColor: "#ff7bacE2", hover: "#f7659aE2" },
   ssr: { bgColor: "#77aa7bE2", hover: "#609363E2" },
-  css: { bgColor: "#FE4A49E2", hover: "#ed3434E2" },
-  testing: { bgColor: "#05afa0E2", hover: "#0ce5d2E2" },
-  webpack: { bgColor: "#a02438", hover: "#a02438E2" },
-  optimization: { bgColor: "#8f4dbf", hover: "#8f4dbfE2" },
   markdown: { bgColor: "#168aad", hover: "#1a759f" },
 };
