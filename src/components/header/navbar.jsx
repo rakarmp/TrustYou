01be41fbfb3172dd8d78d9de684d86b8901618ee
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-12-18 18:48:18 +0700

    add about route

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -61,6 +61,15 @@ const Navbar = () => {
             <a onClick={closeMenu}>Blog</a>
           </NextLink>
         </chakra.li>
+        <chakra.li
+          listStyleType="none"
+          px={{ lg: "8" }}
+          py={{ base: "3", lg: "0" }}
+        >
+          <NextLink href="/about">
+            <a onClick={closeMenu}>About</a>
+          </NextLink>
+        </chakra.li>
       </chakra.ul>
 
       <ColorModeToggle />
